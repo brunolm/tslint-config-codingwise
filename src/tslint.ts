5153d2ab04c5330ec990e43f168e7bc15eff5c23
Bruno Leonardo Michels <brunolm7@gmail.com>
2018-06-21 11:14:20 -0300

    Update tslint.json

--- src/tslint.ts
+++ src/tslint.ts
@@ -1,34 +1,26 @@
-// tslint:disable
+// tslint:disable:no-magic-numbers
 export = {
-  "extends": ["tslint:recommended", "tslint-config-prettier"],
-  "rules": {
-    "cyclomatic-complexity": [true, 10],
-    "interface-name": false,
-    "max-line-length": [true, 120],
-    "member-access": [true],
-    "member-ordering": [
+  extends: ['tslint:recommended', 'tslint-config-prettier'],
+  rules: {
+    'cyclomatic-complexity': [true, 10],
+    'interface-name': false,
+    'member-access': [true],
+    'member-ordering': [
       true,
       {
-        "order": [
-          "static-field",
-          "instance-field",
-          "static-method",
-          "instance-method"
-        ]
-      }
+        order: ['static-field', 'instance-field', 'static-method', 'instance-method'],
+      },
     ],
-    "newline-before-return": true,
-    "no-bitwise": false,
-    "no-conditional-assignment": false,
-    "no-console": [true],
-    "no-magic-numbers": true,
-    "no-non-null-assertion": true,
-    "no-unused-expression": true,
-    "no-unused-variable": true,
-    "object-literal-sort-keys": false,
-    "prefer-object-spread": true,
-    "prefer-template": true,
-    "quotemark": [true, "single", "jsx-double"],
-    "type-literal-delimiter": [true]
-  }
+    'newline-before-return': true,
+    'no-bitwise': false,
+    'no-conditional-assignment': false,
+    'no-magic-numbers': true,
+    'no-non-null-assertion': true,
+    'no-unused-variable': true,
+    'object-literal-sort-keys': false,
+    'prefer-object-spread': true,
+    'prefer-template': true,
+    quotemark: [true, 'single', 'jsx-double'],
+    'type-literal-delimiter': [true],
+  },
 };
